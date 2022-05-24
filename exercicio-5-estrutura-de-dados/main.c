7a97e6ea8519f0a9d4d144dbc6e50e7e1e079bfa
Gustavo Riposati <guriposa@gmail.com>
2022-05-23 21:25:27 -0300

    Update main.c

--- exercicio-5-estrutura-de-dados/main.c
+++ exercicio-5-estrutura-de-dados/main.c
@@ -1,8 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// Função de Ordenação por Seleção
-// Quick sort function
 void quick_sort(int *a, int left, int right) {
     int i, j, x, y;
 
@@ -57,18 +55,6 @@ int main()
         quick_sort(arr, 0, a - 1);
         quick_sort(arr2, 0, a - 1);
 
-        /*for(i=0;i<a;i++){
-            printf("%d ",arr[i]);
-        }
-
-        printf("\n");
-
-        for(i=0;i<a;i++){
-            printf("%d ",arr2[i]);
-        }
-
-        printf("\n");*/
-
         int cont1,cont2,cont3,j;
         cont1 = cont2 = cont3 = 0;
 
@@ -84,24 +70,7 @@ int main()
                 }
         }
 
-           /* if( (i > 0) && (arr2[i] > arr[i-1]) && (arr[i-1] != 0) ){
-                arr[i-1] = 0;
-                cont1++;
-            }
-
-            if( (i < a-1) && (arr2[i] > arr[i+1]) && (arr[i+1] != 0) ){
-                arr[i+1] = 0;
-                cont2++;
-            }
-
-            if( arr2[i] > arr[i] && (arr[i] != 0) ){
-                arr[i] = 0;
-                cont3++;
-            }
-        }*/
-
         printf("%d\n",cont1);
     }
-
     return 0;
 }
